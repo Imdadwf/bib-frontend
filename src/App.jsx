4e611063eafe4bc1e46cbae561ef4cddebec125c
i <i@i.i>
2022-12-14 00:33:02 +0100

    projet react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Musiques from './pages/Musiques'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Bienvenue au GRETA</h1>
+      <Musiques />
+    </div>
+  )
+}
+
+export default App
